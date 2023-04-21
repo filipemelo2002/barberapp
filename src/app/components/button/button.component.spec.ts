3336d3c14dd4e055a8e8d330446ca7052fe32634
Filipe Melo <filipe.silva@encora.com>
2023-04-21 13:21:50 -0300

    feature: add button click & type property biding

--- src/app/components/button/button.component.spec.ts
+++ src/app/components/button/button.component.spec.ts
@@ -1,11 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
+import { EventEmitter } from '@angular/core';
 
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
-
+  let button: HTMLButtonElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ ButtonComponent ]
@@ -14,10 +15,24 @@ describe('ButtonComponent', () => {
 
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
+    button = fixture.nativeElement.querySelector('button') as HTMLButtonElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  fit('should emit click event', () => {
+    component.onClick = jasmine.createSpyObj<EventEmitter<Event>>('onClick', ['emit']);
+    button.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+    expect(component.onClick.emit).toHaveBeenCalled();
+  })
+
+  fit('should render initial type', () => {
+    component.type = 'submit';
+    fixture.detectChanges();
+    expect(button.getAttribute('type')).toEqual('submit')
+  })
 });
--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
+  @Input()
+  type?: string;
 
+  @Output()
+  onClick = new EventEmitter<Event>();
+
+  handleClick($event: Event) {
+    this.onClick.emit($event);
+  }
 }
