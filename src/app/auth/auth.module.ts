cd95cbf7e905393cfd56cf64a23524bc57b422c8
Filipe Melo <filipe.silva@encora.com>
2023-04-21 15:03:23 -0300

    feature: create sign-up page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,19 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { ComponentsModule } from '@components/components.module';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    SignInComponent
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     CommonModule,
     ComponentsModule,
+    RouterModule
   ],
   exports: [
-    SignInComponent
+    SignInComponent,
+    SignUpComponent
   ]
 })
 export class AuthModule { }
