571b9c2fe2fd83730313a617c4f6f8469563886e
Filipe Melo <filipe.silva@encora.com>
2023-04-21 15:05:22 -0300

    chore: fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthModule } from '@auth/auth.module';
 import { SignInComponent } from '@auth/pages/sign-in/sign-in.component';
+import { SignUpComponent } from '@auth/pages/sign-up/sign-up.component';
 
 const routes: Routes = [
   {
-    path: '**', component: SignInComponent
+    path: 'sign-in', component: SignInComponent, title: 'BarberApp - Sign In'
+  },
+  {
+    path: 'sign-up', component: SignUpComponent, title: 'BarberApp - Sign Up'
   }
 ];
 
