16057fe02f31b4cf93e6edaf1946ed3c34a90038
Filipe Melo <filipe.silva@encora.com>
2023-04-30 11:35:38 -0300

    refactor: remove unnecessary console.log statement

--- src/app/auth/use-cases/authorize-customer.service.ts
+++ src/app/auth/use-cases/authorize-customer.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Customer } from '@entities/customer';
+import { HttpClientProviderService } from '@infra/services/http-client-provider.service';
 import { Observable } from 'rxjs';
 
 
@@ -16,12 +16,11 @@ interface  AuthorizeCustomerServiceResponse {
 @Injectable()
 export class AuthorizeCustomerService {
 
-  constructor(private httpClient: HttpClient) { }
-
+  constructor(private httpClient: HttpClientProviderService) { }
 
   execute(request: AuthorizeCustomerServiceRequest): Observable<AuthorizeCustomerServiceResponse> {
     const {email, password} = request;
-    return this.httpClient.post<AuthorizeCustomerServiceResponse>('http://192.168.0.177:3000/login/customer', {
+    return this.httpClient.post<AuthorizeCustomerServiceResponse>('/login/customer', {
       email, password
     }, {withCredentials: true})
   }
