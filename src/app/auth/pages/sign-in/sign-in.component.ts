67acab850a0944de9f989eecf8e09b982a002f89
Filipe Melo <filipe.silva@encora.com>
2023-05-01 13:42:51 -0300

    feature: show error message in sign-in page

--- src/app/auth/pages/sign-in/sign-in.component.ts
+++ src/app/auth/pages/sign-in/sign-in.component.ts
@@ -1,8 +1,16 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthorizeCustomerService } from '@auth/use-cases/authorize-customer.service';
 import { StoreUserService } from '@auth/use-cases/store-user.service';
-import { BehaviorSubject, delay, finalize } from 'rxjs';
+import { ToastService } from '@components/toast/toast.service';
+import { BehaviorSubject, catchError, finalize, throwError } from 'rxjs';
+
+
+export const SIGN_IN_ERROR_MESSAGE_MAP: {[key: number]: string} = {
+  404: 'O E-mail digitado não está cadastrado. Por favor, crie uma conta ou use outro E-Mail.',
+  400: 'E-Mail ou senha inválidos. Por favor, verifique as informações digitadas e tente novamente.'
+};
 
 @Component({
   selector: 'app-sign-in',
@@ -19,7 +27,8 @@ export class SignInComponent {
   constructor(
     private authorizeCustomerService: AuthorizeCustomerService,
     private storeUserService: StoreUserService,
-    private router: Router
+    private router: Router,
+    private toastService: ToastService
   ) {}
 
   signIn() {
@@ -29,7 +38,16 @@ export class SignInComponent {
       password: this.password,
     });
     subscription
-      .pipe(finalize(() => this._loading.next(false)))
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+
+          this.toastService.showError({
+            header: 'Erro ao realizar login.',
+            body: SIGN_IN_ERROR_MESSAGE_MAP[error.status] || 'Por favor, verifique o usuário e senha.'
+          })
+          return throwError(() => error)
+        }),
+        finalize(() => this._loading.next(false)))
       .subscribe((response) => {
         const { customer } = response;
         this.storeUserService.execute(customer);
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,19 +3,28 @@ import { CommonModule } from '@angular/common';
 import { InputComponent } from './input/input.component';
 import { ButtonComponent } from './button/button.component';
 import { FormsModule } from '@angular/forms';
+import { ToastService } from './toast/toast.service';
+import { ToastComponent } from './toast/toast.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     InputComponent,
-    ButtonComponent
+    ButtonComponent,
+    ToastComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    NgbModule
+  ],
+  providers: [
+    ToastService
   ],
   exports: [
     InputComponent,
-    ButtonComponent
+    ButtonComponent,
+    ToastComponent
   ]
 })
 export class ComponentsModule { }
