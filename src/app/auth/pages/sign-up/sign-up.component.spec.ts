167f217239c1c9983fb791752eb9a7d71bdf6a54
Filipe Melo <filipe.silva@encora.com>
2023-05-03 01:06:39 -0300

    feature: add create-user service to sign-up component

--- src/app/auth/pages/sign-up/sign-up.component.spec.ts
+++ src/app/auth/pages/sign-up/sign-up.component.spec.ts
@@ -2,15 +2,28 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignUpComponent } from './sign-up.component';
 import { ComponentsModule } from '@components/components.module';
+import { CreateCustomerService } from '@auth/use-cases/create-customer.service';
+import { InfraModule } from '@infra/infra.module';
+import { of } from 'rxjs';
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
+  let createCustomerService: jasmine.SpyObj<CreateCustomerService>;
   let fixture: ComponentFixture<SignUpComponent>;
 
   beforeEach(async () => {
+    createCustomerService = jasmine.createSpyObj<CreateCustomerService>('createCustomerService', {
+      'execute': of()
+    });
     await TestBed.configureTestingModule({
       declarations: [ SignUpComponent ],
-      imports: [ComponentsModule]
+      imports: [ComponentsModule, InfraModule],
+      providers: [
+        {
+          provide: CreateCustomerService,
+          useValue:createCustomerService
+        }
+      ]
     })
     .compileComponents();
 
@@ -22,4 +35,10 @@ describe('SignUpComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call the signIn method', () => {
+    const button = fixture.nativeElement.querySelector('button') as HTMLButtonElement;
+    button.click();
+    expect(createCustomerService.execute).toHaveBeenCalled();
+  })
 });
--- src/app/auth/pages/sign-up/sign-up.component.ts
+++ src/app/auth/pages/sign-up/sign-up.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { CreateCustomerService } from '@auth/use-cases/create-customer.service';
+import { BehaviorSubject, finalize } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  phone: string = '';
 
+  private _loading = new BehaviorSubject<boolean>(false);
+  public readonly loading$ = this._loading.asObservable();
+
+  constructor(private createCustomer: CreateCustomerService) {}
+  signUp() {
+    this._loading.next(true);
+    const subscription = this.createCustomer.execute({
+      name: this.name,
+      email: this.email,
+      password: this.password,
+      phone: this.phone,
+    });
+
+    subscription.pipe(finalize(() => this._loading.next(false))).subscribe();
+  }
 }
