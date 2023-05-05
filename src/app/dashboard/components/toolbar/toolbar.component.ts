83ac3ceba394c9c810f0345f4d46a7edd6e1a81e
Filipe Melo <filipe.silva@encora.com>
2023-05-04 23:22:21 -0300

    feature: add get-customer-localstorage in Toolbar component

--- src/app/dashboard/components/toolbar/toolbar.component.ts
+++ src/app/dashboard/components/toolbar/toolbar.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CUSTOMER_LOCAL_STORAGE_KEY } from '@constants/index';
+import { GetCustomerLocalstorageService } from '../../user-cases/get-customer-localstorage.service';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {
-  constructor(private router: Router) {
-
+export class ToolbarComponent implements OnInit{
+  name: string = '';
+  constructor(private router: Router, private getCustomerLocalStorage: GetCustomerLocalstorageService) {
   }
+
+  ngOnInit(): void {
+    this.getUserInfo();
+  }
+
+  private getUserInfo() {
+    const response = this.getCustomerLocalStorage.execute();
+    if (!response) {
+      return;
+    }
+
+    this.name = response.customer.name;
+  }
+
   signOut() {
     window.localStorage.removeItem(CUSTOMER_LOCAL_STORAGE_KEY);
     this.router.navigateByUrl('/sign-in');
